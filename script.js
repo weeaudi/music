60813756cb65f2467d743794da21c18f3db98f67
weeaudi <gurskaideng00@isd728.org>
2022-04-25 13:07:54 +0000

    set the size to enitre screen

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /** This is a setup function. */
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(document.documentElement.scrollWidth, document.documentElement.scrollHeight);
 }
 
 /** This is a draw function. */
